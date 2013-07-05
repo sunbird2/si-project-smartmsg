6b76b82839b9cddb95efd5bab3786ca686af395c
si01094457837@gmail.com <si01094457837@gmail.com@7c7cebb1-20b8-c542-b517-de53b9ed5e62>
2013-07-05 06:21:00 +0000

    d

--- trunk/blazeds_4.0/src/com/m/emoticon/EmoticonPagedObject.java
+++ trunk/blazeds_4.0/src/com/m/emoticon/EmoticonPagedObject.java
@@ -4,13 +4,16 @@ public class EmoticonPagedObject {
 
 	private String message;
     private Integer index;
+    private int idx;
+    
     public EmoticonPagedObject() {
         super();
     }
-    public EmoticonPagedObject(String message, Integer index) {
+    public EmoticonPagedObject(String message, Integer index, int idx) {
         super();
         this.message = message;
         this.index = index;
+        this.idx = idx;
     }
     public String getMessage() {
         return message;
@@ -24,4 +27,12 @@ public class EmoticonPagedObject {
     public void setIndex(Integer index) {
         this.index = index;
     }
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+    
+    
 }
