740e9643acd49732fcff6201d9ded0d9c72216b3
si01094457837@gmail.com <si01094457837@gmail.com@7c7cebb1-20b8-c542-b517-de53b9ed5e62>
2013-07-27 02:35:04 +0000

    d

--- trunk/blazeds_4.0/src/com/m/common/Gv.java
+++ trunk/blazeds_4.0/src/com/m/common/Gv.java
@@ -8,7 +8,10 @@ public class Gv {
 	
 	public static HashMap<String, String> COUNT = new HashMap<String, String>();
 	
-	public static String getStatus(String user_id) { return SLibrary.IfNull(Gv.COUNT, user_id); }
+	public static String getStatus(String user_id) {
+		if (SLibrary.isNull(user_id)) return "";
+		else return SLibrary.IfNull(Gv.COUNT, user_id);
+	}
 	public static void setStatus(String user_id , String str) { Gv.COUNT.put(user_id, str); }
 	public static void removeStatus(String user_id) { Gv.COUNT.remove(user_id); }
 
